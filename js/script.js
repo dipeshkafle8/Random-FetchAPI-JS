710477da3c7a89d8322a1274aa236fe5ef3225ec
Dipesh <dipeshkaflen2060@gmail.com>
2024-08-21 23:22:52 +0530

    Random fetch with SweetAlert!

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const fetchData=document.querySelectorAll(".fetchData");
+const displayOutput=document.querySelector("#displayOutput");
+function displayDataOnUI(data){ 
+  console.log(data[0]); 
+  for(let i=0;i<9;i++){
+    let div=document.createElement('div');
+     div.innerText=data[i].title || data[i].name;
+     displayOutput.append(div);
+  }
+}
+
+//alert box using sweetalert
+function displayAlertBoxOnUI(){
+Swal.fire({
+    title: "!!Opps,Wrong EndPoint",   
+    icon: "error",
+    confirmButtonText:"Retry"
+  }).then((result)=>{
+    if(result.isConfirmed){
+      fetchDataFromAPI();
+    }
+  });  
+}
+
+//picking random index and fetching data
+async function fetchDataFromAPI(){
+  displayOutput.innerText="";
+  const apis=["https://jsonplaceholder.typicode.com/users","abc","https://jsonplaceholder.typicode.com/posts"];
+  let index=Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+  let result= await fetch(apis[index]);
+  if(result.status>400){
+    displayAlertBoxOnUI();
+  }
+  else{
+    displayDataOnUI(await result.json());
+  }
+}
+
+//adding eventListeners to the button
+fetchData.forEach((el)=>{
+    el.addEventListener('click',()=>{
+      console.log("Button clicked");
+        fetchDataFromAPI();
+    })
+})
+
+
+
+
